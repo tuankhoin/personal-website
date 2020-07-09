c18ce19b12af5df56465c903917930b5ba56ef19
tuankhoin <tuankhoin@student.unimelb.edu.au>
2020-07-09 15:04:13 +1000

    navigation images

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from "react"
 
 export default function Header(props) {
     return (
-    <div style={{ textAlign: 'center',
+    <div class = "header" style={{ textAlign: 'center',
                   verticalAlign: 'center',
                   marginTop: '15rem',
                   marginBottom: '15rem' }}> 
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,33 @@ export default function Home() {
       children={<a href="#start" class="btn btn-info" role="button">Let's go!</a>} />
 
       <section id = "start">
-        <img data-aos="fade-right" src="https://source.unsplash.com/random/400x200" alt="" />
-        <img data-aos="fade-right" src="https://source.unsplash.com/random/400x200" alt="" />
+        <div class="icontainer" style={{paddingTop: 20}}>
+          <div class="ibox">
+            <div class = "img" id = "ab" data-aos="fade-right"><a href="/about/">Who am I?</a></div>   
+          </div>
+          
+          <div class="ibox">
+            <div class = "img" id = "pj" data-aos="fade-right"><a href="/projects/">Projects</a></div>   
+          </div>
+        </div>
+        <div class="icontainer" style={{marginTop: -7}}>
+          <div class="ibox">
+          <div class = "img" id = "pt" data-aos="fade-right"><a href="/photography/">Photo Gallery</a></div>   
+          </div>
+          
+          <div class="ibox">
+            <div class = "img" id = "ms" data-aos="fade-right"><a href="/music/">Music Gallery</a></div>            
+          </div>
+        </div>
+        <div class="icontainer" style={{marginTop: -7}}>
+          <div class="ibox">
+          <div class = "img" id = "ct" data-aos="fade-right"><a href="/contact/" id="bottom">Contact</a></div>   
+          </div>
+          
+          <div class="ibox">
+            <div class = "img" id = "end" data-aos="fade-right"><a href="/" id="bottom">That's all!</a></div>            
+          </div>
+        </div>
       </section>
     </Layout>
   )
