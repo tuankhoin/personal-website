52c3466962d2fed0dd3f47f53790643cb0a09ae0
tuankhoin <tuankhoin@student.unimelb.edu.au>
2020-07-11 23:40:58 +1000

    //

--- src/pages/music.js
+++ src/pages/music.js
@@ -12,6 +12,10 @@ export default function Music() {
       <Container>
       <Header headerText="Music gallery!" caption = {<a href="#start" className="btn btn-info" role="button">Continue</a>}/>
       </Container>
+
+      <section id = "start">
+      <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Ftuankhoi.nguyen.16%2Fvideos%2F1804036812962572%2F&show_text=true&width=734&height=522&appId" width="734/2" height="522/2" style={{border:"none",overflow:"hidden"}} scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" allowFullScreen="true"></iframe>
+      </section>
   </Layout>
   )
 }
