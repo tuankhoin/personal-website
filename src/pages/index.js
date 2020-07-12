5a3249a1039e4ffd45d6ca2dec72f7f51e6e5e81
tuankhoin <tuankhoin@student.unimelb.edu.au>
2020-07-12 15:32:29 +1000

    indexing

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,35 +12,36 @@ export default function Home() {
       <Container><Header headerText="Hello stranger!" caption = "Let's get started, shall we?"
       children={<a href="#start" className="btn btn-info" role="button">Let's scroll down!</a>} />
       </Container>
-      <section id = "start" style={{padding: "0%", width: "100%", marginLeft:"0%",borderRadius:0}}>
+      <section id = "start" style={{padding: "0%", width: "100%", marginLeft:"0%",borderRadius:0,boxShadow:"none"}}>
         <p style={{fontSize:'1.2em', color:'rgb(6, 11, 35)'}}>Select one: </p>
-        <div className="icontainer" style={{paddingTop: 0}}>
-          <div className="ibox">
+        <div className="row" style={{paddingTop: 0}}>
+          <div className="col-lg ibox">
             <div className = "img" id = "ab"><Link to="/about/">Who am I?</Link></div>   
           </div>
           
-          <div className="ibox">
+          <div className="col-lg ibox">
             <div className = "img" id = "pj"><Link to="/projects/">Projects & Publications</Link></div>   
           </div>
         </div>
-        <div className="icontainer" style={{marginTop: 0}}>
-          <div className="ibox">
+        <div className="row" style={{marginTop: 0}}>
+          <div className="col-lg ibox">
           <div className = "img" id = "pt"><Link to="/photography/">Photo Gallery</Link></div>   
           </div>
           
-          <div className="ibox">
+          <div className="col-lg ibox">
             <div className = "img" id = "ms"><Link to="/music/">Music Gallery</Link></div>            
           </div>
         </div>
-        <div className="icontainer" style={{marginTop: -0}}>
+        <div className="row" style={{marginTop: -0}}>
           
-          <div className="ibox">
+          <div className="col-lg ibox">
+            <div className = "img" id = "ct"><Link to="/contact/" id="bottom">Contact</Link></div>   
+          </div>
+
+          <div className="col-lg ibox">
             <div className = "img" id = "end"><Link to="/" id="bottom">That's all!</Link></div>            
           </div>
 
-          <div className="ibox">
-            <div className = "img" id = "ct"><Link to="/contact/" id="bottom">Contact</Link></div>   
-          </div>
         </div>
       </section>
     </Layout>
